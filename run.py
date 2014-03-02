4bd9d74f97c26a125110aec74a1b2063f6d86a61
Sean Cascketta <scascketta@gmail.com>
2014-03-02 14:06:26 -0600

    Modify run script for direct execution

--- run.py
+++ run.py
@@ -1,18 +1,26 @@
+#!/usr/bin/env python
+from logging import FileHandler, Formatter
 from app import app
 import logging
 import time
 import os
-from logging import FileHandler, Formatter
 
-if not os.path.exists('logs/'):
-    os.mkdir('logs')
-logpath = os.getcwd() + '/logs/' + time.asctime() + '.log'
-datefmt = '%m/%d/%g@%H:%M'
-fh = FileHandler(logpath)
-fh.setLevel(logging.DEBUG)
-fmt = Formatter('%(asctime)s:%(levelname)s - %(module)s:%(funcName)s - %(message)s')
-fmt.datefmt = datefmt
-fh.setFormatter(fmt)
-app.logger.addHandler(fh)
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+def config_logging(log_dname="logs"):
+	log_dname = "{0}/".format(log_dname)
+	if not os.path.exists(log_dname):
+	    os.mkdir(log_dname)
+	logpath = os.getcwd() + "/{0}/".format(log_dname) + time.asctime() + '.log'
+	datefmt = '%m/%d/%g@%H:%M'
+	fh = FileHandler(logpath)
+	fh.setLevel(logging.DEBUG)
+	fmt = Formatter('%(asctime)s:%(levelname)s - %(module)s:%(funcName)s - %(message)s')
+	fmt.datefmt = datefmt
+	fh.setFormatter(fmt)
+	app.logger.addHandler(fh)
+	return app
+
+
+if __name__ == '__main__':
+	app = config_logging()
+	app.run(host='0.0.0.0', port=5000, debug=True)
