837e9d769297d95e639961b113d6856e350b791c
Sean Cascketta <scascketta@utexas.edu>
2014-03-02 16:08:26 -0600

    Expand tabs to spaces

--- run.py
+++ run.py
@@ -7,20 +7,20 @@ import os
 
 
 def config_logging(log_dname="logs"):
-	log_dname = "{0}/".format(log_dname)
-	if not os.path.exists(log_dname):
-	    os.mkdir(log_dname)
-	logpath = os.getcwd() + "/{0}/".format(log_dname) + time.asctime() + '.log'
-	datefmt = '%m/%d/%g@%H:%M'
-	fh = FileHandler(logpath)
-	fh.setLevel(logging.DEBUG)
-	fmt = Formatter('%(asctime)s:%(levelname)s - %(module)s:%(funcName)s - %(message)s')
-	fmt.datefmt = datefmt
-	fh.setFormatter(fmt)
-	app.logger.addHandler(fh)
-	return app
+    log_dname = "{0}/".format(log_dname)
+    if not os.path.exists(log_dname):
+        os.mkdir(log_dname)
+    logpath = os.getcwd() + "/{0}/".format(log_dname) + time.asctime() + '.log'
+    datefmt = '%m/%d/%g@%H:%M'
+    fh = FileHandler(logpath)
+    fh.setLevel(logging.DEBUG)
+    fmt = Formatter('%(asctime)s:%(levelname)s - %(module)s:%(funcName)s - %(message)s')
+    fmt.datefmt = datefmt
+    fh.setFormatter(fmt)
+    app.logger.addHandler(fh)
+    return app
 
 
 if __name__ == '__main__':
-	app = config_logging()
-	app.run(host='0.0.0.0', port=5000, debug=True)
+    app = config_logging()
+    app.run(host='0.0.0.0', port=5000, debug=True)
